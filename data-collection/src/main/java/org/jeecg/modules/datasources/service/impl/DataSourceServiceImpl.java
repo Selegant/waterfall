f3fe8b71769c0ff8f961979653d96d8df3ff6365
detective <13750846673@163.com>
2021-05-24 11:01:55 +0800

    获取tables修改判断

--- data-collection/src/main/java/org/jeecg/modules/datasources/service/impl/DataSourceServiceImpl.java
+++ data-collection/src/main/java/org/jeecg/modules/datasources/service/impl/DataSourceServiceImpl.java
@@ -1,25 +1,16 @@
 package org.jeecg.modules.datasources.service.impl;
 
-import cn.hutool.db.Db;
 import cn.hutool.db.DbRuntimeException;
-import cn.hutool.db.DbUtil;
 import cn.hutool.db.ds.simple.SimpleDataSource;
-import cn.hutool.db.meta.Column;
 import cn.hutool.db.meta.MetaUtil;
-import cn.hutool.db.meta.Table;
-import cn.hutool.db.meta.TableType;
-import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.sql.DataSource;
-
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.jeecg.modules.datasources.dto.TableColumnInfoDTO;
-import org.jeecg.modules.datasources.dto.WaterfallDataSourceListDTO;
 import org.jeecg.modules.datasources.input.TableColumnInput;
 import org.jeecg.modules.datasources.mapper.WaterfallDataSourceMapper;
 import org.jeecg.modules.datasources.mapper.WaterfallDataSourceTypeMapper;
@@ -28,8 +19,6 @@ import org.jeecg.modules.datasources.model.WaterfallDataSourceType;
 import org.jeecg.modules.datasources.service.IDataSourceService;
 import org.jeecg.modules.datasources.util.DBUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.List;
 import org.springframework.stereotype.Service;
 
 /**
@@ -58,17 +47,18 @@ public class DataSourceServiceImpl implements IDataSourceService {
     @Override
     public List<WaterfallDataSource> list(String purpose) {
         QueryWrapper<WaterfallDataSource> wrapper = new QueryWrapper<>();
-        wrapper.eq("purpose",purpose);
-       return waterfallDataSourceMapper.selectList(wrapper);
+        wrapper.eq("purpose", purpose);
+        return waterfallDataSourceMapper.selectList(wrapper);
     }
 
     @Override
     public List<WaterfallDataSourceType> dataSourceTypeList() {
-        return waterfallDataSourceTypeMapper.selectList(new QueryWrapper<WaterfallDataSourceType>());
+        return waterfallDataSourceTypeMapper
+                .selectList(new QueryWrapper<WaterfallDataSourceType>());
     }
 
     @Override
-    public void updateDataSource(WaterfallDataSource dataSource) throws Exception{
+    public void updateDataSource(WaterfallDataSource dataSource) throws Exception {
         dataSource.setJdbcUrl(concatUrl(dataSource));
         connection(dataSource);
         waterfallDataSourceMapper.updateByPrimaryKeySelective(dataSource);
@@ -95,17 +85,18 @@ public class DataSourceServiceImpl implements IDataSourceService {
     }
 
     @Override
-    public Boolean connection(WaterfallDataSource dataSource) throws Exception{
+    public Boolean connection(WaterfallDataSource dataSource) throws Exception {
         dataSource.setJdbcUrl(concatUrl(dataSource));
-        DataSource db = new SimpleDataSource(dataSource.getJdbcUrl(), dataSource.getUsername(), dataSource.getPassword());
+        DataSource db = new SimpleDataSource(dataSource.getJdbcUrl(), dataSource.getUsername(),
+                dataSource.getPassword());
         try {
-            if (MetaUtil.getTables(db,dataSource.getDbName()).size() > 0) {
+            if (MetaUtil.getTables(db, dataSource.getDbName()).size() > 0) {
                 return true;
             } else {
                 throw new RuntimeException("请设置具体数据库");
             }
-        }catch (DbRuntimeException e){
-            log.error(e.getMessage(),e);
+        } catch (DbRuntimeException e) {
+            log.error(e.getMessage(), e);
             throw new RuntimeException("数据库连接失败");
         }
 
@@ -113,28 +104,39 @@ public class DataSourceServiceImpl implements IDataSourceService {
 
     @Override
     public List<String> getTables(WaterfallDataSource dataSource) throws SQLException {
-      DataSource db = new SimpleDataSource(dataSource.getJdbcUrl(), dataSource.getUsername(), dataSource.getPassword());
-      return MetaUtil.getTables(db,dataSource.getDbName());
+        dataSource.setJdbcUrl(concatUrl(dataSource));
+        DataSource db = new SimpleDataSource(dataSource.getJdbcUrl(), dataSource.getUsername(),
+                dataSource.getPassword());
+        if (MYSQL.equals(dataSource.getDbType())) {
+            return MetaUtil.getTables(db, dataSource.getDbName());
+        }
+        if (ORACLE.equals(dataSource.getDbType())) {
+            return MetaUtil.getTables(db, dataSource.getServerName());
+        }
+        return new ArrayList<>();
     }
 
     @Override
     public List<TableColumnInfoDTO> getTableColumns(TableColumnInput input) {
-      DataSource db = new SimpleDataSource(input.getJdbcUrl(), input.getUsername(), input.getPassword());
-      return DBUtil.getTableMeta(db,input.getTableName(),input.getDatabase());
+        DataSource db = new SimpleDataSource(input.getJdbcUrl(), input.getUsername(),
+                input.getPassword());
+        return DBUtil.getTableMeta(db, input.getTableName(), input.getDatabase());
     }
 
-    private String concatUrl(WaterfallDataSource dataSource) throws RuntimeException{
-        if(!StringUtils.isNotBlank(dataSource.getDbType())){
+    private String concatUrl(WaterfallDataSource dataSource) throws RuntimeException {
+        if (!StringUtils.isNotBlank(dataSource.getDbType())) {
             throw new RuntimeException("dbType不能为空");
         }
         String type = dataSource.getDbType().toLowerCase();
-        if(MYSQL.equals(type)){
+        if (MYSQL.equals(type)) {
             String mysqlUrl = "jdbc:mysql://%s:%s/%s";
-            return String.format(mysqlUrl, dataSource.getHost(),dataSource.getPort(),dataSource.getDbName());
+            return String.format(mysqlUrl, dataSource.getHost(), dataSource.getPort(),
+                    dataSource.getDbName());
         }
-        if(ORACLE.equals(type)){
+        if (ORACLE.equals(type)) {
             String oracleUrl = "jdbc:oracle:thin:@%s:%s/%s";
-            return String.format(oracleUrl, dataSource.getHost(),dataSource.getPort(),dataSource.getServerName());
+            return String.format(oracleUrl, dataSource.getHost(), dataSource.getPort(),
+                    dataSource.getServerName());
         }
         return "";
     }
