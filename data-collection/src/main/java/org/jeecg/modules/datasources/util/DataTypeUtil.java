08b2c93de3166d871e31280bd67aad5d3d411a1b
selegant <1228579928@qq.com>
2021-08-03 16:19:07 +0800

    添加数据类型解析工具类

---
+++ data-collection/src/main/java/org/jeecg/modules/datasources/util/DataTypeUtil.java
@@ -0,0 +1,64 @@
+package org.jeecg.modules.datasources.util;
+
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.*;
+
+/**
+ * @author selegant
+ */
+public class DataTypeUtil {
+
+    public final static Map<String, String> MYSQL_HIVE =new HashMap<String, String>() {
+        {
+            put("TINYINT","TINYINT");
+            put("SMALLINT","SMALLINT");
+            put("MEDIUMINT","INT");
+            put("INT","INT");
+            put("BIGINT","BIGINT");
+            put("FLOAT","FLOAT");
+            put("DOUBLE","DOUBLE");
+            put("DECIMAL","DECIMAL");
+            put("NUMERIC","DECIMAL");
+            put("VARCHAR","STRING");
+            put("CHAR","STRING");
+            put("TIMESTAMP","TIMESTAMP");
+            put("DATETIME","DATE");
+        }
+    };
+
+
+
+    public final static Map<String, String> ORACLE_HIVE =new HashMap<String, String>() {
+        {
+            put("NUMBER","INT");
+            put("BINARY_FLOAT","FLOAT");
+            put("BINARY_DOUBLE","DOUBLE");
+            put("VARCHAR","STRING");
+            put("CHAR","STRING");
+            put("RAW","BINARY");
+            put("TIMESTAMP","TIMESTAMP");
+            put("DATE","DATE");
+        }
+    };
+
+
+
+    public final static Map<String, Map<String, String>> DATA_MAPPING =new HashMap<String, Map<String, String>>() {
+        {
+            put("Mysql",MYSQL_HIVE);
+            put("Oracle",ORACLE_HIVE);
+        }
+    };
+
+
+
+    public static List<JSONObject> parseDataType(String dbType, List<JSONObject> list){
+        Map<String, String> mapping = DATA_MAPPING.get(dbType);
+        list.forEach(s->{
+            s.put("targetType",mapping.get(s.getString("originalType").toUpperCase(Locale.ROOT)));
+        });
+        return list;
+    }
+
+}
